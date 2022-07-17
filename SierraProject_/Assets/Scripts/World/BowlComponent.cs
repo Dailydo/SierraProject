3b3cc840573b1f526e895fa1fa9568548402e9ab
Romz89 <romain.colnot@gmail.com>
2022-07-17 14:45:12 +0200

    adding bowl component & behaviour + link bowls to doors in game

---
+++ SierraProject_/Assets/Scripts/World/BowlComponent.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class BowlComponent : ObstacleComponent
+{
+    [SerializeField]
+    private DoorComponent m_linkedDoor = null;
+
+    protected override void OnInteractedInternal(PlayerComponent player)
+    {
+        base.OnInteractedInternal(player);
+
+        if (m_linkedDoor != null)
+            m_linkedDoor.SetUsed();
+    }
+}
